55e45e4528be7bfd1b5daa5badf1f04530268131
汪一沛 <39909797+Geiv@users.noreply.github.com>
2019-03-16 17:41:43 +0800

    Switch smaller binary txts to larger ones.
    To resolve the 1200x1200 binary txt problems, switch them to 2048x2048 pngs and txts.

---
+++ small_txt2png&bin.py
@@ -0,0 +1,45 @@
+from PIL import Image, ImageDraw
+# 规定2048 x 2048画纸:
+width = 2048
+height = 2048
+
+# 最大为5460：
+txt_num = int(input('输入读取txt的数量,最大为5460，最小为2:'))
+
+for k in range(1, txt_num):
+    image = Image.new('RGB', (width, height), (255, 255, 255))
+    small_bin_image = [[] for i in range(1200)]
+    bin_image = [[] for i in range(2048)]
+    # read the bin.txt:
+    with open('train/ac' + str(k) + '.txt', 'r') as f:
+        line = f.readlines()
+        for i in range(1200):
+            for j in line[i]:
+                if j == '0' or j == '1':
+                    small_bin_image[i].append(int(j))
+        '''
+        for i in range(512):
+            bin_image[i] = [0 for i in range(2048)]
+        temp = 0
+        for i in range(512, 1712):
+            bin_image[i] = [0 for i in range(512)] + small_bin_image[temp] + [0 for i in range(336)]
+            temp += 1
+        for i in range(1712, 2048):
+            bin_image[i] = [0 for i in range(2048)]
+        '''
+
+    # fill the pixels:
+    for y in range(height):
+        for x in range(width):
+            if bin_image[x][y] == 1:
+                ImageDraw.Draw(image).point((y, x), fill=(255, 0, 0))
+    # output the png:
+    image.save('Mask_png/' + str(k) + '.png', 'png')
+    print('Mask_png/' + str(k) + '.png, done.')
+    # output the txt:
+    with open('Mask_bin/' + str(k) + '.txt', 'w') as f:
+        for i in bin_image:
+            for j in i:
+                f.write(str(j) + ' ')
+            f.write('\n')
+    print('Mask_bin/' + str(k) + '.txt, done.')
